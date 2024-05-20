75e2bf136e043bc656b572ea65d5d5d419f05835
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-05-20 10:10:38 +0900

    feat: Bun.serve Http App

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello Bun!')
+import type { Server } from 'bun'
+
+const hostname = process.env.BUN_HOST ?? 'localhost'
+const port = process.env.BUN_PORT ?? 3000
+
+Bun.serve({
+  hostname,
+  port,
+  fetch(request: Request, server: Server): Response | Promise<Response> {
+    const ip = server.requestIP(request)
+    return new Response(`Your IP is ${JSON.stringify(ip, null, 2)}`)
+  },
+})
+
+console.log('Server is running on http://localhost:3000/')
